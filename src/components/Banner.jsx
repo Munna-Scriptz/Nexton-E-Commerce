1350970e3f7c5071ac50660892935a6afd8ed039
Munna-Scripts <munnascriptz@gmail.com>
2025-07-06 14:54:50 +0600

    Slider Complete

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
 import BannerImg from "../assets/Images/banner.png"
+import BannerImg2 from "../assets/Images/Banner2.png"
 
 const Banner = () => {
   // -----Slider Fun----
@@ -15,6 +16,9 @@ const Banner = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     adaptiveHeight: true,
+    autoplay: true,
+    speed: 2000,
+    pauseOnHover: true
   }
 
   return (
@@ -33,11 +37,11 @@ const Banner = () => {
             </div>
           </section>
           <section className='w-full h-[600px]' >
-            <div style={{backgroundImage:`url(${BannerImg})` , backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center',}}>
+            <div style={{backgroundImage:`url(${BannerImg2})` , backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center',}}>
               <div className="container">
                 <div id="Banner_Text" className='py-[148px]'>
-                    <h2 className='font-medium text-[20px] text-Primary'>Starting from: $49.99</h2>
-                    <h1 className='font-semibold text-[64px] leading-[72px] w-[632px] text-[#111827] mb-[24px] mt-[18px]'>Exclusive collection for everyone</h1>
+                    <h2 className='font-medium text-[20px] text-Primary'>100% Original Products</h2>
+                    <h1 className='font-semibold text-[64px] leading-[72px] w-[632px] text-[#111827] mb-[24px] mt-[18px]'>The All New Fashion Collection</h1>
                     <Link className='text-white py-[20px] px-[36px] bg-second rounded-full flex items-center w-fit gap-2.5' to={'/'}>Explore now <CiSearch className='text-[20px]'/></Link>
                 </div>
               </div>
