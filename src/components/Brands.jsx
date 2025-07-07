ddefa626868aed97d5f11e46ec0193ce722b97b1
Munna-Scripts <munnascriptz@gmail.com>
2025-07-07 17:35:13 +0600

    Responsive Slider Complete without Dots

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -10,19 +10,54 @@ export const Brands = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        }
+      }
+    ]
   }
 
   return (
     <>
 
-    <section id='Brands_Slider' className='mt-[52px] mb-6 lg:mt-[88px] lg:mb-[40px]'>
+    <section id='Brands_Slider' className='mt-[52px] mb-6 lg:mt-[88px] lg:mb-[40px] pb-20 px-[20px] lg:px-0'>
       <div className="container">
         {/* --------Common Head-------- */}
         <div className='mb-[40px]'><CommonHead StartText={'Start exploring.'} EndText={'Good things are waiting for you'}/></div>
 
         {/* --------Slider-------- */}
-          <div>
+          <div className='cursor-pointer'>
             <div className="slider-container">
               <Slider {...settings}>
                 <SingleBrand BrandHead={`For Men's`} BrandP={`Starting at $24`}/>
--- src/components/common/SingleBrand.jsx
+++ src/components/common/SingleBrand.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { Link } from 'react-router';
 
 const SingleBrand = ({BrandHead , BrandP}) => {
   return (
     <section>
-        <div className=' lg:border-[1px] border-[#E5E7EB] p-[40px] rounded-[16px] flex justify-between items-center w-[419px]'>
+        <div className='border-[1px] border-[#E5E7EB] px-[24px] py-[40px] lg:p-[40px] rounded-[16px] flex justify-between items-center w-full lg:w-[419px]'>
             <div>
-                <h2 className='text-second text-2xl font-semibold mb-[3px]'>{BrandHead}</h2>
+                <h2 className='text-second text-[20px] lg:text-2xl font-semibold mb-[3px]'>{BrandHead}</h2>
                 <p className='text-Primary text-sm'>{BrandP}</p>
             </div>
-            <div className='flex items-center gap-[12px]'>
-                <span className='border-[#E5E7EB] lg:border-[1px] lg:h-[50px] w-[1px]'></span>
-                <p className='flex items-center gap-[8px] text-Primary font-medium'>Shop Now <IoIosArrowRoundForward className='text-[26px]'/></p>
+            <div className='flex items-center gap-[6px] lg:gap-[12px]'>
+                <span className='border-[#E5E7EB] border-[1px] h-[32px] w-[2px]'></span>
+                <Link to={'/'} className='flex items-center gap-[8px] text-Primary font-medium'>Shop Now <IoIosArrowRoundForward className='text-[26px]'/></Link>
             </div>
         </div>
     </section>
