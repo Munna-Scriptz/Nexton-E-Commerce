0266f36b4534f42ef9c208b299feee6051e19fcc
Munna-Scripts <munnascriptz@gmail.com>
2025-07-08 12:59:16 +0600

    Best Seller Section Complete

---
+++ src/components/BestSeller.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Slider from "react-slick";
+import { CommonHead } from './common/CommonHead'
+import SingleProducts from './common/SingleProducts';
+
+const BestSeller = () => {
+
+    const settings = {
+    infinite: true,
+    slidesToShow: 3,
+    speed: 500,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        }
+      }
+    ]
+  };
+
+  return (
+    <section id='Recommend' className='mt-[176px] ml-[24px] lg:ml-0'>
+            <div className="container">
+                <CommonHead StartText={'Best Sellers.'} EndText={' Best selling of the month'}/>
+                
+                <div className="slider-container">
+                    <Slider {...settings}>
+                        <SingleProducts/>
+                        <SingleProducts/>
+                        <SingleProducts/>
+                        <SingleProducts/>
+                        <SingleProducts/>
+                        <SingleProducts/>
+                        <SingleProducts/>
+    
+                    </Slider>
+                </div>
+    
+            </div>
+        </section>
+  )
+}
+
+export default BestSeller
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,16 @@ import Banner from '../components/Banner'
 import Service from '../components/Service'
 import { Brands } from '../components/Brands'
 import Recommend from '../components/Recommend'
+import BestSeller from '../components/BestSeller'
 
 export const Home = () => {
   return (
     <>
-    
     <Banner/>
     <Service/>
     <Brands/>
     <Recommend/>
+    <BestSeller/>
     </>
   )
 }
