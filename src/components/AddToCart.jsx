30cd403cb3ff3b1a6afd0d3545f0c562b5b11585
Munna-Scripts <munnascriptz@gmail.com>
2025-07-12 21:50:30 +0600

    Product Details Added

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -7,13 +7,25 @@ export const AddToCart = () => {
     <>
         <section id='Add-To-Cart' className='h-full bg-[#f8f8f8] w-[500px] p-5 border-l-2'>
             {/* ------Add to Cart Header */}
-            <div className='flex items-center justify-between'>
+            <div className='flex items-center justify-between mb-10'>
                 <h2 className='font-medium text-[20px] text-second flex items-center gap-4'>Add To Cart <RiShoppingCart2Line className='text-[24px] text-Primary'/>
                 </h2>
                 <div className='cursor-pointer'><RxCross2 className='text-3xl'/></div>
             </div>
             {/* ------Add items */}
-
+            <div className='flex items-center justify-between'>
+                <div className='flex items-center gap-5'>
+                    <div className='w-[60px] h-[60px] bg-Primary rounded-[10px]'></div>
+                    <div>
+                        <h2 className='text-second font-medium text-[18px]'>Product Name</h2>
+                        <p className='text-Primary font-normal text-base'>Product Details</p>
+                    </div>
+                </div>
+                <div>
+                    <h2 className='text-second font-medium text-[18px]'>$50</h2>
+                    <p className='text-Primary font-normal text-base line-through'>69$</p>
+                </div>
+            </div>
         </section>
     </>
   )
