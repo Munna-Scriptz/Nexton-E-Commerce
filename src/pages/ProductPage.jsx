9a40148e49ce70c7e9ec851b2c963d1bbad7d3bd
Munna-Scripts <munnascriptz@gmail.com>
2025-07-15 18:59:54 +0600

    Products Left Side COmplete

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -16,28 +16,28 @@ const ProductPage = () => {
                                 <label for='check' className='text-Primary text-sm'>Men’s fashion</label>
                             </div>
                             <div  className='flex items-center gap-3 mt-[16px]'>
-                                <input type="checkbox" className='w-5 h-5 border-2 rounded-[4px] outline-none border-[#94A3B8]' id='check'/>
-                                <label for='check' className='text-Primary text-sm'>Women’s fashion</label>
+                                <input type="checkbox" className='w-5 h-5 border-2 rounded-[4px] outline-none border-[#94A3B8]' id='check1'/>
+                                <label for='check1' className='text-Primary text-sm'>Women’s fashion</label>
                             </div>
                             <div  className='flex items-center gap-3 mt-[16px]'>
-                                <input type="checkbox" className='w-5 h-5 border-2 rounded-[4px] outline-none border-[#94A3B8]' id='check'/>
-                                <label for='check' className='text-Primary text-sm'>Kids & Toys</label>
+                                <input type="checkbox" className='w-5 h-5 border-2 rounded-[4px] outline-none border-[#94A3B8]' id='check2'/>
+                                <label for='check2' className='text-Primary text-sm'>Kids & Toys</label>
                             </div>
                             <div  className='flex items-center gap-3 mt-[16px]'>
-                                <input type="checkbox" className='w-5 h-5 border-2 rounded-[4px] outline-none border-[#94A3B8]' id='check'/>
-                                <label for='check' className='text-Primary text-sm'>Accessories</label>
+                                <input type="checkbox" className='w-5 h-5 border-2 rounded-[4px] outline-none border-[#94A3B8]' id='check3'/>
+                                <label for='check3' className='text-Primary text-sm'>Accessories</label>
                             </div>
                             <div  className='flex items-center gap-3 mt-[16px]'>
-                                <input type="checkbox" className='w-5 h-5 border-2 rounded-[4px] outline-none border-[#94A3B8]' id='check'/>
-                                <label for='check' className='text-Primary text-sm'>Cosmetics</label>
+                                <input type="checkbox" className='w-5 h-5 border-2 rounded-[4px] outline-none border-[#94A3B8]' id='check4'/>
+                                <label for='check4' className='text-Primary text-sm'>Cosmetics</label>
                             </div>
                             <div  className='flex items-center gap-3 mt-[16px]'>
-                                <input type="checkbox" className='w-5 h-5 border-2 rounded-[4px] outline-none border-[#94A3B8]' id='check'/>
-                                <label for='check' className='text-Primary text-sm'>Shoes</label>
+                                <input type="checkbox" className='w-5 h-5 border-2 rounded-[4px] outline-none border-[#94A3B8]' id='check5'/>
+                                <label for='check5' className='text-Primary text-sm'>Shoes</label>
                             </div>
                             <div  className='flex items-center gap-3 mt-[16px]'>
-                                <input type="checkbox" className='w-5 h-5 border-2 rounded-[4px] outline-none border-[#94A3B8]' id='check'/>
-                                <label for='check' className='text-Primary text-sm'>Sports</label>
+                                <input type="checkbox" className='w-5 h-5 border-2 rounded-[4px] outline-none border-[#94A3B8]' id='check6'/>
+                                <label for='check6' className='text-Primary text-sm'>Sports</label>
                             </div>
                         </div>
                         {/* Range */}
@@ -49,18 +49,42 @@ const ProductPage = () => {
                                     <div>
                                         <p className='text-Primary font-normal text-base mb-1'>Min price</p>
                                         <div className='flex items-center justify-between w-[130px] py-2 px-[16px] border-1 border-[#E5E7EB] rounded-full'>
-                                            <p className='text-Primary font-medium text-sm'>100</p>
+                                            <input type="number" className='placeholder:text-Primary w-full h-full border-none outline-none text-Primary font-medium text-sm' placeholder='100'/>
                                             <p className='text-Primary font-medium text-sm'>$</p>
                                         </div>
                                     </div>
                                     <div>
                                         <p className='text-Primary font-normal text-base mb-1'>Max price</p>
                                         <div className='flex items-center justify-between w-[130px] py-2 px-[16px] border-1 border-[#E5E7EB] rounded-full'>
-                                            <p className='text-Primary font-medium text-sm'>500</p>
+                                            <input type="number" className='placeholder:text-Primary w-full h-full border-none outline-none font-medium text-sm' placeholder='500'/>
                                             <p className='text-Primary font-medium text-sm'>$</p>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+                        {/* Sort Order */}
+                        <div className=' mt-[32px]'>
+                            <h2 className='text-[18px] font-semibold text-second font-poppins mb-[24px]'>Sort order</h2>
+                            <div  className='flex items-center gap-3'>
+                                <input type="radio" className='w-5 h-5 rounded-full' id='radio1'/>
+                                <label for='radio1' className='text-Primary text-sm'>Most Popular</label>
+                            </div>
+                            <div  className='flex items-center gap-3 mt-[16px]'>
+                                <input type="radio" className='w-5 h-5 rounded-full border-2outline-none border-[#94A3B8]' id='radio2'/>
+                                <label for='radio2' className='text-Primary text-sm'>Best Rating</label>
+                            </div>
+                            <div  className='flex items-center gap-3 mt-[16px]'>
+                                <input type="radio" className='w-5 h-5 rounded-full border-2 outline-none border-[#94A3B8]' id='radio3'/>
+                                <label for='radio3' className='text-Primary text-sm'>Most Popular</label>
+                            </div>
+                            <div  className='flex items-center gap-3 mt-[16px]'>
+                                <input type="radio" className='w-5 h-5 rounded-full border-2outline-none border-[#94A3B8]'id='radio4'/>
+                                <label for='radio4' className='text-Primary text-sm'>Price Low - Hight</label>
+                            </div>
+                            <div  className='flex items-center gap-3 mt-[16px]'>
+                                <input type="radio" className='w-5 h-5 rounded-full border-2outline-none border-[#94A3B8]' id='radio5'/>
+                                <label for='radio5' className='text-Primary text-sm'>Price Hight - Low</label>
                             </div>
                         </div>
                     </div>
