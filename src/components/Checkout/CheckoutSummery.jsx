c83e24ddf6ffbc929b6bd0c2e69dbcbfd985ffd1
Munna-Scripts <munnascriptz@gmail.com>
2025-08-04 18:44:32 +0600

    Checkout Page Function Complete

--- src/components/Checkout/CheckoutSummery.jsx
+++ src/components/Checkout/CheckoutSummery.jsx
@@ -2,17 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { IoResize } from "react-icons/io5";
 import axios from 'axios';
 
-const CheckoutSummery = ({CheckImg , CheckPrice , CheckName , HideInSmTop , HideInSmBottom , HideAllInLG}) => {
-  // -------First Quantity
-  const [value , setValue] = useState(1)
-  if(value < 1){
-    setValue(value + 1)
-  }
-  // -------second Quantity
-  const [value2 , setValue2] = useState(1)
-  if(value2 < 1){
-    setValue2(value2 + 1)
-  }
+const CheckoutSummery = ({HideInSmTop , HideInSmBottom , HideAllInLG}) => {
   // ----------------------------------------------------------Hooks 
   const ProductID = JSON.parse(localStorage.getItem('product'))
   const [Product, SetProduct] = useState([])
@@ -35,25 +25,43 @@ const CheckoutSummery = ({CheckImg , CheckPrice , CheckName , HideInSmTop , Hide
     })
     .catch((err)=>{console.log(err)})
   },[])
+  // ---------------------------------------------------------Handle Quantity 
   
-  // --------------------Handle Quantity
-  const handleQty = (id)=>{
+  const handleQty = (id) =>{
     SetProduct((prev)=>(
       prev.map((item)=>{
         if(item.id != id) return item
-        const UpdatedQty = item.qty + 1
-        const UpdatedPrice = item.unitePrice * UpdatedQty
+        const updatedQty = item.qty + 1
+        const updatedPrice = item.unitePrice * updatedQty
 
-        return {...item, qty:UpdatedQty , price: UpdatedPrice}
+        return {...item , qty:updatedQty , price: updatedPrice}
       })
-
     ))
   }
+  const handleQtyMinus = (id) =>{
+    SetProduct((prev)=>(
+      prev.map((item)=>{
+        if(item.id != id) return item
 
-  const TotalPrice = Product.reduce((sum , total)=>{
-    return sum + total.price
+        if(item.price < 1){
+          const updatedQty = item.qty + 0
+          const updatedPrice = item.unitePrice * updatedQty
+          return {...item , qty:updatedQty , price: updatedPrice}
+        }
+        else{
+
+          const updatedQty = item.qty - 1
+          const updatedPrice = item.unitePrice * updatedQty
+          return {...item , qty:updatedQty , price: updatedPrice}
+        }
+      })
+    ))
+  }
+  
+  const TotalPrice = Product.reduce((sum , product)=>{
+    return sum + product.price
   }, 0)
-
+  // --------------------Handle Quantity
   return (
     <>
       <section id='Checkout-Summery' className={`lg:w-[618px] lg:px-0 w-full px-[24px] ${HideAllInLG}`}>
@@ -74,7 +82,7 @@ const CheckoutSummery = ({CheckImg , CheckPrice , CheckName , HideInSmTop , Hide
                     <h2 className='font-semibold text-base text-second'>{item.title}</h2>
                     <p className='text-Primary text-sm font-normal flex items-center gap-1 mt-1'><IoResize /> One size</p>
                     <div className='flex items-center gap-4 mt-[20px]'>
-                      <button className='w-[24px] h-[24px] border-2 border-[#E5E7EB] rounded-full text-[18px] text-second cursor-pointer hover:bg-Primary hover:text-white duration-[.3s] select-none' onClick={()=>handleQty(item.id)}>-</button>
+                      <button className='w-[24px] h-[24px] border-2 border-[#E5E7EB] rounded-full text-[18px] text-second cursor-pointer hover:bg-Primary hover:text-white duration-[.3s] select-none' onClick={()=>handleQtyMinus(item.id)}>-</button>
                       <p className='text-base text-Primary font-medium'>{item.qty}</p>
                       <button className='w-[24px] h-[24px] border-2 border-[#E5E7EB] rounded-full text-[18px] text-second cursor-pointer hover:bg-Primary hover:text-white duration-[.3s] select-none' onClick={()=>handleQty(item.id)}>+</button>
                     </div>
@@ -89,7 +97,7 @@ const CheckoutSummery = ({CheckImg , CheckPrice , CheckName , HideInSmTop , Hide
           }
         </div>
           {/* -------Checkout final--------- */}
-          <div className={`mt-6 ${HideInSmBottom}`}>
+          <div className={`mt-6 ${HideInSmBottom} select-none`}>
             <p className='SubtotalPrice text-base text-Primary font-normal flex items-center justify-between'>Subtotal <span className='truncate text-clip w-13'>${TotalPrice}</span></p>
             <p className='text-base text-Primary font-normal flex items-center justify-between mt-2'>Shipping estimate <span>$5.00</span></p>
             <p className='text-base text-Primary font-normal flex items-center justify-between mt-2'>Tax estimate <span>$24.90</span></p>
